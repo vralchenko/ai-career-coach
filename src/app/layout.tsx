eb7ec955817cc9ae90a322f18af530145084b69b
Viktor Ralchenko <viktorr@finera.com>
2025-12-25 18:53:03 +0100

    Added copy right.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
 import { ThemeProvider } from '@/components/ThemeProvider';
+import { Footer } from '@/components/Footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -23,7 +24,12 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
         >
-            {children}
+            <div className="min-h-screen bg-slate-50 dark:bg-[#0a0a0c]">
+                <main className="pb-20">
+                    {children}
+                </main>
+                <Footer />
+            </div>
         </ThemeProvider>
         </body>
         </html>
